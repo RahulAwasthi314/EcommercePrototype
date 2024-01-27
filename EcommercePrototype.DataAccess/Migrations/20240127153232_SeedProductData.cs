305c204b4da02fba5047c90d4003ef2fb91939a2
Rahul Awasthi <awasthir314@gmail.com>
2024-01-27 21:04:16 +0530

    Add Migration to Seed Data in Product Table

---
+++ EcommercePrototype.DataAccess/Migrations/20240127153232_SeedProductData.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EcommercePrototype.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Polo tshirts are best to wear at summer. Provides sweatfree days in hot days", "Polo Tshirt", 100m },
+                    { 2, "Formal shirts are all weather shirts that can be used for daily office fashion", "Formal Shirt", 300m },
+                    { 3, "The shoes that you can wear while playing sports, walking, running or jogging", "Sport shoes", 600m },
+                    { 4, "Prmosie your eyes to protect them from dust and dirt", "Eye protecting glasses", 100m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+        }
+    }
+}
